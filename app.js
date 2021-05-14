689b1ecca60875628e513832eecc00301fa6c975
yuvaraj-1991 <yuviatudemy@gmail.com>
2021-05-14 16:53:53 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,4 @@
+import '@patternfly/pfe-navigation';
+import '@patternfly/pfe-card';
+import '@patternfly/pfe-card/dist/pfe-card.min.js';
+import "https://unpkg.com/@patternfly/pfe-card/dist/pfe-card.min.js";
